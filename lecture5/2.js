a63ec8730916e8f2187fb6992a20fa3849671ace
Vladimir Myltcev <ntwrite@gmail.com>
2022-11-17 08:23:43 +0100

    add 5.2

---
+++ lecture5/2.js
@@ -0,0 +1,27 @@
+const data = [
+  { text: 'First', type: 'static' },
+  { text: 1, type: 'static' },
+  { text: Math.PI, type: 'static' },
+  { type: 'dynamic', text: () => 'Dynamic text#3' },
+  { text: 'Second', type: 'static' },
+  { text: [], type: 'static' },
+  { text: 'Third', type: 'static' },
+  { type: 'dynamic', text: () => 'Dynamic text#2' },
+  { text: 'Last', type: 'static' },
+  { text: { text: 9 }, type: 'static' },
+  { type: 'dynamic', text: () => 'Dynamic text#1' },
+];
+
+let items = [];
+
+// ================== Your code below ==================
+// Note: Вы можете обращаться к элементу массива по индексу, например items[2] -> { text: Math.PI },
+// Напишите код, который переложит из data в items только те элементы у которых type === dynamic
+// Вам понадобятся циклы, условия, switch и немного знаний о том, как обращаться к свойствам объекта и вызывать функции
+
+
+
+
+// ================== End of your code ==================
+
+renderList(data);
